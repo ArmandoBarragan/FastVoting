c694a292ba266179d0408dc1c3ef5c5bb9989ad2
Armando Barragan <armandobp765@gmail.com>
2022-07-20 15:14:08 -0600

    fixed main.py and environment problems

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -1,16 +1,15 @@
 import os
 
-# Import settings
-from src.settings import container, local
 
-env = os.environ
-DEBUG = env.get('DEBUG')
+DEBUG = os.getenv('DEBUG', default=True)
 
-settings = local if not DEBUG else container
 
-# Assign variables
-DEBUG = settings.DEBUG
-HOST = settings.HOST
-PORT = settings.PORT
+# APP
+HOST = os.getenv('HOST', default='0.0.0.0')
+PORT = os.getenv('PORT', default=8000)
 
-SECRET_KEY = settings.SECRET_KEY
+# DATABASE
+DB_HOST = os.getenv('DB_HOST', default='localhost')
+DB_PORT = os.getenv('DB_PORT', default=5432)
+
+SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KLMASFUIHNQEGYRN')
